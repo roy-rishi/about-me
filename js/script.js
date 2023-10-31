f9769183682ad93abd1a605a890575bd157190e4
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2023-10-31 14:30:09 -0700

    nav bar ui enhancements
    * add box shadow
    * increase opacity

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ if (document.getElementById('my-work-link')) {
 }
 
 // set nav bar background opacity and blur by scroll
-const PIXELS_TO_SCROLL=200;
+const PIXELS_TO_SCROLL=50;
 const MAX_BLUR=30;
+const MAX_OPACITY=0.85;
+const MAX_OPACITY_SHADOW=0.22
 
 const navbar = document.querySelector('#navbar');
 function handleScroll() {
@@ -16,7 +18,10 @@ function handleScroll() {
   const opacity = Math.min(scrollPosition / PIXELS_TO_SCROLL, 1);
   const blur = Math.min(scrollPosition / PIXELS_TO_SCROLL * MAX_BLUR, MAX_BLUR);
 
-  navbar.style.backgroundColor = `rgba(255, 255, 255, ${opacity * .5})`;
+  // opacity
+  navbar.style.backgroundColor = `rgba(255, 255, 255, ${opacity * MAX_OPACITY})`;
+  navbar.style.boxShadow = `0px 1px 5px rgba(100, 100, 100, ${opacity * MAX_OPACITY_SHADOW})`;
+  // blur
   navbar.style.backdropFilter = `blur(${blur}px)`;
   navbar.style.webkitBackdropFilter = `blur(${blur}px)`;
 }

e539c7494aed248f797e01f20e8c2575302f5ba6
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2023-11-09 17:56:13 -0800

    fix bug that required reload with javascript

--- js/load-projects.js
+++ js/load-projects.js
@@ -126,7 +126,7 @@ const content = JSON.parse(
                     "type": "paragraph",
                     "heading": "Computer-Aided Design",
                     "content": [
-                        "<span style='font-style: italic; font-weight: 800;'>Click and drag</span> to the left or right to interact with the model below. Please reload the page if the area below appears blank.",
+                        "<span style='font-style: italic; font-weight: 800;'>Click and drag</span> to the left or right to interact with the model below",
                         "<iframe src='${imgBaseURL}violin-robot/36-frame-table.html' width='100%' height='640px' frameborder='0' scrolling='no'>"
                     ]
                 },
